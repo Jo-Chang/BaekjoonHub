41ee520e1b5a2d08db4f774af2d98e18707a5486
Jo-Chang <74034844+Jo-Chang@users.noreply.github.com>
2023-01-03 17:46:15 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    /*
+             ,r'"7
+    r`-_   ,'  ,/
+     \. ". L_r'
+       `~\/
+          |
+          |
+    */
+    cout << "         ,r\'\"7" << endl;
+    cout << "r`-_   ,\'  ,/" << endl;
+    cout << " \\. \". L_r\'" << endl;
+    cout << "   `~\\/" << endl;
+    cout << "      |" << endl;
+    cout << "      |" << endl;
+    
+    return 0;
+}
