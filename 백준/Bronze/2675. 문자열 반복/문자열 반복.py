ef4b5df9d528dd18d3f9f52f263aa57cc76b7e70
Jo-Chang <74034844+Jo-Chang@users.noreply.github.com>
2023-01-18 13:44:26 +0900

    [Bronze II] Title: 문자열 반복, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.py
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.py
@@ -1,6 +1,4 @@
 for test_case in range(1, int(input()) + 1):
     repeat_num, word = input().split()
     repeat_num = int(repeat_num)
-    for i_char in word:
-        print(i_char * repeat_num, end="")
-    print()
+    print(*[i_char*repeat_num for i_char in word], sep="")
