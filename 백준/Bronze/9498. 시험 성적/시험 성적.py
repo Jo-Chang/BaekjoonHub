79600c839b40733cffeb6e98f8eaf88f59540318
Jo-Chang <74034844+Jo-Chang@users.noreply.github.com>
2023-01-18 13:52:59 +0900

    [Bronze V] Title: 시험 성적, Time: 36 ms, Memory: 30748 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,12 @@
+score_num = int(input())
+
+if score_num >= 90:
+    print("A")
+elif score_num >= 80:
+    print("B")
+elif score_num >= 70:
+    print("C")
+elif score_num >= 60:
+    print("D")
+else:
+    print("F")
