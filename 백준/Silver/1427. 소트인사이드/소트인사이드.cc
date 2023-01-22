bc6fb201f09b99f994c71848668400ac951a8497
Jo-Chang <74034844+Jo-Chang@users.noreply.github.com>
2023-01-22 19:34:07 +0900

    [Silver V] Title: 소트인사이드, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.cc
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.cc
@@ -5,8 +5,6 @@
 
 using namespace std;
 
-bool desc(int a, int b);
-
 int main()
 {
     vector<char> v;
@@ -17,15 +15,10 @@ int main()
     {
         v.push_back(word[i]);
     }
-    sort(v.begin(), v.end(), desc);
+    sort(v.begin(), v.end(), greater<char>());
     
     for (int i = 0; i < v.size(); i++)
         cout << v[i];
     
     return 0;
-}
-
-bool desc(int a, int b)
-{
-    return a > b;
 }
