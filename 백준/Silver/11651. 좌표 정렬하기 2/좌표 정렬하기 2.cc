81c26d42edd0b9f2dbf0c38f0cfbb9bcf7fff909
Jo-Chang <74034844+Jo-Chang@users.noreply.github.com>
2023-01-23 14:38:34 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 120 ms, Memory: 2808 KB -BaekjoonHub

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
@@ -7,26 +7,26 @@ using namespace std;
 
 typedef pair<int, int> pii;
 
-bool sort_y(pii a, pii b);
-void print_vector(vector<pii> v);
+bool sort_y(pii, pii);
+void print_arr(pii*, int);
 
 int main()
 {
-    vector<pii> point_vec;
-    int n_num;
-    
+    pii *point_arr;
+    int n_num, x_pos, y_pos;
+
     cin >> n_num;
+    point_arr = new pii[n_num];
     for (int i = 0; i < n_num; i++)
     {
-        int x_pos, y_pos;
         cin >> x_pos >> y_pos;
-        point_vec.push_back(pii(x_pos, y_pos));
+        point_arr[i] = pii(x_pos, y_pos);
     }
-    
-    sort(point_vec.begin(), point_vec.end(), sort_y);
-    
-    print_vector(point_vec);
-    
+
+    sort(point_arr, point_arr + n_num, sort_y);
+
+    print_arr(point_arr, n_num);
+
     return 0;
 }
 
@@ -37,8 +37,8 @@ bool sort_y(pii a, pii b)
     return a.second < b.second;
 }
 
-void print_vector(vector<pii> v)
+void print_arr(pii *pii_arr, int N)
 {
-    for (int i = 0; i < v.size(); i++)
-        cout << v[i].first << " " << v[i].second << "\n";
+    for (int i = 0; i < N; i++)
+        cout << pii_arr[i].first << " " << pii_arr[i].second << "\n";
 }
