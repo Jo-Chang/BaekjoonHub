f7428c89e3f2176846967babaa3f97fb2e466194
Jo-Chang <74034844+Jo-Chang@users.noreply.github.com>
2023-01-23 14:39:49 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 48 ms, Memory: 2808 KB -BaekjoonHub

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
@@ -1,44 +1,28 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
-
 using namespace std;
-#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-
-typedef pair<int, int> pii;
-
-bool sort_y(pii, pii);
-void print_arr(pii*, int);
-
-int main()
-{
-    pii *point_arr;
-    int n_num, x_pos, y_pos;
-
-    cin >> n_num;
-    point_arr = new pii[n_num];
-    for (int i = 0; i < n_num; i++)
-    {
-        cin >> x_pos >> y_pos;
-        point_arr[i] = pii(x_pos, y_pos);
-    }
-
-    sort(point_arr, point_arr + n_num, sort_y);
-
-    print_arr(point_arr, n_num);
-
-    return 0;
-}
-
-bool sort_y(pii a, pii b)
-{
-    if (a.second == b.second)
-        return a.first < b.first;
+bool compare(pair<int, int> a, pair<int,int> b){
+  if(a.second == b.second){
+    return a.first < b.first;
+  }
+  else{
     return a.second < b.second;
+  }
 }
 
-void print_arr(pii *pii_arr, int N)
-{
-    for (int i = 0; i < N; i++)
-        cout << pii_arr[i].first << " " << pii_arr[i].second << "\n";
+int main() {
+  cin.tie(NULL);
+  ios_base::sync_with_stdio(false);
+  int N,x,y;
+  cin >> N;
+  pair<int, int> *point = new pair<int, int>[N];
+  for(int i=0; i<N; i++){
+    cin >> x >> y;
+    point[i].first = x;
+    point[i].second = y;
+  }
+  sort(point, point+N, compare);
+  for(int i=0; i<N; i++){
+    cout << point[i].first << " " << point[i].second << '\n';
+  }
 }
