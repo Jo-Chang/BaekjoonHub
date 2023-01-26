52b77ee3be8bd22ac0f9568d7d88b9a9ba50fab8
Jo-Chang <74034844+Jo-Chang@users.noreply.github.com>
2023-01-26 14:41:26 +0900

    [Silver IV] Title: 괄호, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -0,0 +1,22 @@
+import sys
+
+def is_correct_VPS(sentence):
+    stack = []
+    for ch in sentence:
+        if ch == ")":
+            try:
+                stack.pop()
+            except:
+                return "NO"
+        else:
+            stack.append(ch)
+    
+    if len(stack) == 0:
+        return "YES"
+    else:
+        return "NO"
+
+
+for T in range(1, int(sys.stdin.readline()) + 1):
+    # print(f"#{T} : ")
+    print(is_correct_VPS(sys.stdin.readline().strip()))
