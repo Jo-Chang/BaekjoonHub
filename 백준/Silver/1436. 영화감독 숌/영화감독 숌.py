3f1d873ab2d42d2d0f11946b01a7150a10fb1265
Jo-Chang <74034844+Jo-Chang@users.noreply.github.com>
2023-02-01 16:06:38 +0900

    [Silver V] Title: 영화감독 숌, Time: 416 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -5,12 +5,11 @@ def get_666_num(num):
     ans = 666
     cnt = 0
     while True:
-        ans = str(ans)
-        if "666" in ans:
+        if "666" in str(ans):
             cnt += 1
             if cnt == num:
-                return int(ans)
-        ans = int(ans) + 1
+                return ans
+        ans += 1
 
 
 print(get_666_num(int(sys.stdin.readline())))
