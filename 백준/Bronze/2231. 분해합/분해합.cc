2c9919b7815e249dfea9e37d7c5afab1d04a5da3
Jo-Chang <74034844+Jo-Chang@users.noreply.github.com>
2023-02-11 09:56:43 +0900

    [Bronze II] Title: 분해합, Time: 8 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+
+int get_min_constructor(int);
+int main()
+{
+    fastio;
+    int num_n;
+    
+    cin >> num_n;
+    
+    cout << get_min_constructor(num_n);
+    
+    return 0;
+}
+
+int get_min_constructor(int num)
+{
+    int num_constructor, num_digit = 0;
+    int num_temp = num;
+    
+    // Get num digit
+    while (num_temp > 0)
+    {
+        num_digit += 1;
+        num_temp /= 10;
+    }
+    
+    // Maximum start value when num is digit
+    num_constructor = pow(10, num_digit - 1) - 9 * num_digit;
+    
+    // Find minimum constructor num
+    for (int i = num_constructor; i <= num; i++)
+    {
+        int num_sum = i;
+        num_temp = i;
+        
+        while (num_temp > 0)
+        {
+            num_sum += num_temp % 10;
+            num_temp /= 10;
+        }
+        
+        // Compare construct number with num
+        if (num_sum == num)
+            return i;
+    }
+    
+    // No constructor return 0
+    return 0;
+}
