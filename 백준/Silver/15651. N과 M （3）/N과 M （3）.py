7dd84fcc38b8388110db0711241ca23d2246db1d
Jo-Chang <74034844+Jo-Chang@users.noreply.github.com>
2023-03-15 22:23:15 +0900

    [Silver III] Title: N과 M (3), Time: 1992 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.py
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -1,15 +1,15 @@
 # 15651. N과 M (3)
 
 n, m = map(int, input().split())
+lst = []
 
-def my_comb_recursion(n: int, k: int, lst: list):  
+def my_comb_recursion(n: int, k: int):  
     if k == 0:
         print(*lst)
-        lst.pop()
         return 
     for i in range(1, n+1):
         lst.append(i)
-        my_comb_recursion(n, k-1, lst)
-    if k != m: lst.pop()
+        my_comb_recursion(n, k-1)
+        lst.pop()
     
-my_comb_recursion(n, m, [])
+my_comb_recursion(n, m)
