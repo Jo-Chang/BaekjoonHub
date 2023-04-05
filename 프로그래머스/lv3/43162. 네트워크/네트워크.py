f1fed9595435dc68cd4d457a5601b2e023e0a773
Jo-Chang <74034844+Jo-Chang@users.noreply.github.com>
2023-04-05 18:01:42 +0900

    [level 3] Title: 네트워크, Time: 0.19 ms, Memory: 9.97 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.py
@@ -0,0 +1,20 @@
+def solution(n, computers):
+    answer = 0
+    visited = [False] * n
+    
+    for i in range(n):
+        if visited[i]:
+            continue
+        
+        answer += 1
+        stack = [i]
+        visited[i] = True
+        while stack:
+            v = stack.pop()
+            print(v)
+            for j in range(n):
+                if computers[v][j] and not visited[j]:
+                    visited[j] = True
+                    stack.append(j)
+                
+    return answer
