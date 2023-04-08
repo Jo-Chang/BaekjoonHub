2514d225829532ae1d9cccb2a23b24f10e3c6a10
Jo-Chang <74034844+Jo-Chang@users.noreply.github.com>
2023-04-08 15:15:51 +0900

    [Silver I] Title: 안전 영역, Time: 1112 ms, Memory: 39884 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.py
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -1,7 +1,7 @@
 # 2468. 안전 영역
 
 import sys
-# import pprint
+from pprint import pprint
 sys.setrecursionlimit(10 ** 4)
 
 delta = [
@@ -12,38 +12,68 @@ delta = [
 ]
 
 def check_valid(pos: tuple, n: int):
-    # global area_temp
-    if 0 <= pos[0] < n and 0 <= pos[1] < n and area_temp[pos[0]][pos[1]] > 0:
-        return True
+    global area_temp
+    
+    if 0 <= pos[0] < n and 0 <= pos[1] < n :
+        if area_temp[pos[0]][pos[1]] > 0:
+            return True
     return False
 
-
 def dfs(pos: tuple):
-    # global delta, area_temp
-    for dx, dy in delta:
+    global delta, area_temp
+    
+    for dy, dx in delta:
         new_pos = (pos[0]+dy, pos[1]+dx)
         if check_valid(new_pos, n):
             area_temp[new_pos[0]][new_pos[1]] = 0
             dfs(new_pos)
 
+def bfs(pos: tuple):
+    global delta, area_temp
+    
+    lst = list()
+    for dy, dx in delta:
+        new_pos = (pos[0]+dy, pos[1]+dx)
+        if check_valid(new_pos, n):
+            area_temp[new_pos[0]][new_pos[1]] = 0
+            lst.append(new_pos)
+    
+    for pos_ in lst:
+        bfs(pos_)
+
+def dfs_loop(position: tuple):
+    global delta, area_temp
+    stack = [position]
+    
+    while stack:
+        pos = stack.pop()
+
+        for dy, dx in delta:
+            new_pos = (pos[0]+dy, pos[1]+dx)
+            if check_valid(new_pos, n):
+                area_temp[new_pos[0]][new_pos[1]] = 0
+                stack.append(new_pos)
+    
 
 n = int(input())
 area = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
-# pprint.pprint(area)
 heights = set(sum(area, []))
 
 answer = 1
 for height in heights:
     cnt = 0
-    area_temp = [[i - height for i in area[j]] for j in range(n)]
+    area_temp = [[i - height for i in line] for line in area]
     for i in range(n):
         for j in range(n):
             if area_temp[i][j] <= 0:
                 continue
             cnt += 1
-            dfs((i, j))
-    
+            
+            area_temp[i][j] = 0
+            # dfs((i, j))
+            # bfs((i, j))
+            dfs_loop((i, j))
+            
     answer = cnt if cnt > answer else answer
     
 print(answer)
-        
