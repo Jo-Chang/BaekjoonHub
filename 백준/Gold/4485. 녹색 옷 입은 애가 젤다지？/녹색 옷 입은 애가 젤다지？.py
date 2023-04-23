3671d5bcfdf412fbaf349b2e84542f93b2657e42
Jo-Chang <74034844+Jo-Chang@users.noreply.github.com>
2023-04-24 00:25:18 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 136 ms, Memory: 33324 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.py
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.py
@@ -17,23 +17,30 @@ while True:
     cost = [[int(1e9)] * N for _ in range(N)]
 
     q = []
-    cost[0][0] = 0
-    heapq.heappush(q, (map_[0][0], (0, 0)))
+    heapq.heappush(q, (map_[0][0], 0, 0))
     
     while q:
-        cur_cost, cur_pos = heapq.heappop(q)
+        cur_cost, y, x = heapq.heappop(q)
         
-        if cur_pos[0] == N-1 and cur_pos[1] == N-1:
+        if y == N-1 and x == N-1:
             answer = cur_cost
             break
         
         for dy, dx in DELTA:
-            new_pos = (cur_pos[0]+dy, cur_pos[1]+dx)
-            if not (0 <= new_pos[0] < N and 0 <= new_pos[1] < N):
+            ny, nx = y + dy, x + dx
+            if not (0 <= ny < N and 0 <= nx < N):
                 continue
-            new_cost =  map_[new_pos[0]][new_pos[1]] + cur_cost
-            if map_[new_pos[0]][new_pos[1]] != VISITED and cost[new_pos[0]][new_pos[1]] > new_cost:
-                cost[new_pos[0]][new_pos[1]] = new_cost
-                heapq.heappush(q, (new_cost, new_pos))
+            new_cost =  map_[ny][nx] + cur_cost
+            if map_[ny][nx] != VISITED and cost[ny][nx] > new_cost:
+                cost[ny][nx] = new_cost
+                heapq.heappush(q, (new_cost, ny, nx))
+        # for dy, dx in DELTA:
+        #     new_pos = (cur_pos[0]+dy, cur_pos[1]+dx)
+        #     if not (0 <= new_pos[0] < N and 0 <= new_pos[1] < N):
+        #         continue
+        #     new_cost =  map_[new_pos[0]][new_pos[1]] + cur_cost
+        #     if map_[new_pos[0]][new_pos[1]] != VISITED and cost[new_pos[0]][new_pos[1]] > new_cost:
+        #         cost[new_pos[0]][new_pos[1]] = new_cost
+        #         heapq.heappush(q, (new_cost, new_pos))
 
     print(f'Problem {test_case}: {answer}')
