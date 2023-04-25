58e9a78aa29253f7f06cf5c16b04c38e4442e7ec
Jo-Chang <74034844+Jo-Chang@users.noreply.github.com>
2023-04-25 16:35:24 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 560 ms, Memory: 34324 KB -BaekjoonHub

--- 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.py
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.py
@@ -2,21 +2,27 @@
 
 import sys
 
+
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
 
-lst = [list(map(int, input().split())) for _ in range(N)]
-
+lst = []
+for i in range(N):
+    line = list(map(int, input().split()))
+    for idx, num in enumerate(line):
+        if idx:
+            line[idx] = line[idx-1] + num
+    lst.append(line)
+    
 K = int(input())
 
-answer = list()
 for _ in range(K):
-    cnt = 0
+    answer = 0
     i, j, x, y = map(int, input().split())
-    
-    cnt = sum([sum(lst[l][j-1:y]) for l in range(i-1, x)])
-    
-    answer.append(cnt)
-            
-print('\n'.join(map(str, answer)))
+    for p in range(i-1, x):
+        answer += lst[p][y-1]
+        if j > 1:
+            answer -= lst[p][j-2]
+          
+    print(answer)
