95caedeffa0a7195cfcf9340d4c6f0e4e87da8e4
Jo-Chang <74034844+Jo-Chang@users.noreply.github.com>
2023-05-06 17:51:58 +0900

    [Bronze II] Title: 분산처리, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/1009. 분산처리/분산처리.py
@@ -0,0 +1,26 @@
+# 1009. 분산처리
+
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+for _ in range(T):
+    a, b = map(int, input().split())
+    a %= 10
+    if a == 0:
+        print(10)
+    elif a == 2 or a == 3 or a == 7 or a == 8:
+        # 2, 3, 7, 8
+        # 2, 4, 8, 6, 2, ...
+        # 3, 9, 7, 1, 3, ...
+        # 7, 9, 3, 1, 7, ...
+        # 8, 4, 2, 6, 8, ...
+        print(pow(a, (b-1)%4+1) % 10)
+    elif a == 4 or a == 9:
+        # 4, 9
+        # 4, 6, 4, ...
+        # 9, 1, 9, ...
+        print(pow(a, (b-1)%2+1) % 10)
+    else:
+        # 1, 5, 6
+        print(a)
