beed73a1e318474d98ea89d0d06ef652bcfff8d0
Jo-Chang <74034844+Jo-Chang@users.noreply.github.com>
2023-08-30 11:41:28 +0900

    [Silver III] Title: 파도반 수열, Time: 124 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.py
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.py
@@ -4,27 +4,26 @@ import sys
 fin = sys.stdin.readline
 
 MAX_N = 100
-SEQ_P = [0] * MAX_N
+seq_p = [0] * MAX_N
 
 
 def wave_sequence():
-    global SEQ_P
-    
-    SEQ_P[0] = SEQ_P[1] = SEQ_P[2] = 1
-    SEQ_P[3] = SEQ_P[4] = 2
+    global seq_p
+    seq_p[0] = seq_p[1] = seq_p[2] = 1
+    seq_p[3] = seq_p[4] = 2
     
     for i in range(5, MAX_N):
-        SEQ_P[i] = SEQ_P[i - 1] + SEQ_P[i - 5]
+        seq_p[i] = seq_p[i - 1] + seq_p[i - 5]
     
 def solution():
-    global SEQ_P
+    global seq_p
     T = int(fin())
     
     wave_sequence()
     
     for _ in range(T):
         N = int(fin())
-        print(SEQ_P[N - 1])
+        print(seq_p[N - 1])
         
 
 if __name__ == "__main__":
