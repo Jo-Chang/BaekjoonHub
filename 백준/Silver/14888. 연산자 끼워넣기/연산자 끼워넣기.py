eea5a5ccfe8122c04a13c8c8347776e03f5e087b
Jo-Chang <74034844+Jo-Chang@users.noreply.github.com>
2023-08-31 14:53:12 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 228 ms, Memory: 117320 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -2,6 +2,8 @@ import sys
 
 
 fin = sys.stdin.readline
+max_num = -1000000000
+min_num = 1000000000
 result = list()
 
     
@@ -47,7 +49,35 @@ def solution():
     calculate(1, cal_dict, num_lst[0])
     
     print(max(result), min(result), sep='\n')
+
+
+# Faster
+def solution2():
+    def calculate(idx: int, plus: int, minus: int, multi: int, div: int, calculating: int):
+        global max_num, min_num
+        
+        if idx == N:
+            max_num = calculating if max_num < calculating else max_num
+            min_num = calculating if min_num > calculating else min_num
+        else:
+            if plus:
+                calculate(idx + 1, plus - 1, minus, multi, div, calculating + num_lst[idx])
+            if minus:
+                calculate(idx + 1, plus, minus - 1, multi, div, calculating - num_lst[idx])
+            if multi:
+                calculate(idx + 1, plus, minus, multi - 1, div, calculating * num_lst[idx])
+            if div:
+                calculate(idx + 1, plus, minus, multi, div - 1, int(calculating / num_lst[idx]))
+
+    N = int(fin())
+    num_lst = list(map(int, fin().rstrip().split()))
+    cal_lst = list(map(int, fin().rstrip().split()))
+    
+    calculate(1, cal_lst[0], cal_lst[1], cal_lst[2], cal_lst[3], num_lst[0])
+    
+    print(max_num, min_num, sep='\n')
     
     
 if __name__ == "__main__":
-    solution()
+    # solution()
+    solution2()
