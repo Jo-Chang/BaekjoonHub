a8a50c7bbd640f35639fbba5e6dba934945c5919
Jo-Chang <74034844+Jo-Chang@users.noreply.github.com>
2023-09-02 16:33:22 +0900

    [Silver III] Title: 수열, Time: 140 ms, Memory: 125556 KB -BaekjoonHub

---
+++ 백준/Silver/2559. 수열/수열.py
@@ -0,0 +1,19 @@
+import sys
+
+
+fin = sys.stdin.readline
+
+N, K = map(int, fin().split())
+S = list(map(int, fin().split()))
+
+for i in range(1, N):
+    S[i] += S[i - 1]
+
+temp=list()
+S.append(0)
+max_sum = -1e9
+for i in range(N - K + 1):
+    num_sum = S[i + K - 1] - S[i - 1]
+    max_sum = num_sum if num_sum > max_sum else max_sum
+
+print(max_sum)
