1a220e1c22db1902fba071971c9ae464cb4dc53d
Jo-Chang <74034844+Jo-Chang@users.noreply.github.com>
2023-09-02 14:52:07 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 232 ms, Memory: 125440 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
@@ -9,9 +9,11 @@ num_lst = list(map(int, fin().split()))
 for i in range(1, N):
     num_lst[i] += num_lst[i - 1]
     
+num_lst.append(0)
 for _ in range(M):
     i, j = map(int, fin().split())
-    if i > 1:
-        print(num_lst[j - 1] - num_lst[i - 2])
-    else:
-        print(num_lst[j - 1])
+    print(num_lst[j - 1] - num_lst[i - 2])
+    # if i > 1:
+        # print(num_lst[j - 1] - num_lst[i - 2])
+    # else:
+        # print(num_lst[j - 1])
