16a58a3d00a9e7c10b9c4fe5b6d10045bdd71b1b
Jo-Chang <74034844+Jo-Chang@users.noreply.github.com>
2023-09-04 12:13:54 +0900

    [Silver II] Title: 스타트와 링크, Time: 272 ms, Memory: 115456 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -14,12 +14,10 @@ for team in combinations(range(1, N), N // 2 - 1):
     team1_cnt = 0
     team2_cnt = 0
     
-    for i in range(N):
-        for j in range(i+1, N):
-            if i in team1 and j in team1:
-                team1_cnt += ability[i][j] + ability[j][i]
-            elif i in team2 and j in team2:
-                team2_cnt += ability[i][j] + ability[j][i]
+    for i in range(N // 2):
+        for j in range(i + 1, N // 2):
+            team1_cnt += ability[team1[i]][team1[j]] + ability[team1[j]][team1[i]]
+            team2_cnt += ability[team2[i]][team2[j]] + ability[team2[j]][team2[i]]
     
     min_diff = min(abs(team1_cnt - team2_cnt), min_diff)
     if not min_diff:
