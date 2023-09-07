e306260026422fb3c1c15f74c95e09848bd0741a
Jo-Chang <74034844+Jo-Chang@users.noreply.github.com>
2023-09-07 13:24:40 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 224 ms, Memory: 15916 KB -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.StringTokenizer;
+import java.util.Arrays;
 
 public class Main {
   public static void main(String[] args) throws IOException {
@@ -9,21 +10,25 @@ public class Main {
 
     String word = br.readLine();
     int q = Integer.parseInt(br.readLine());
-    char a;
+    char ch;
     int l, r;
+
+    int[][] cntArr = new int[word.length() + 1][26];  // 알파뱃 26문자
+    Arrays.fill(cntArr[0], 0);
+    for (int i = 1; i <= word.length(); i++) {
+      for (int j = 0; j < 26; j++) {
+        cntArr[i][j] = cntArr[i - 1][j];
+      }
+      cntArr[i][word.charAt(i - 1) - 'a']++;
+    }
 
     while (q-- > 0) {
       StringTokenizer st = new StringTokenizer(br.readLine());
-      a = st.nextToken().charAt(0);
+      ch = st.nextToken().charAt(0);
       l = Integer.parseInt(st.nextToken());
       r = Integer.parseInt(st.nextToken());
 
-      int cnt = 0;
-      for (int i = l; i <= r; i++) {
-        if (word.charAt(i) == a) cnt++;
-      }
-
-      System.out.println(cnt);
+      System.out.println(cntArr[r + 1][ch - 'a'] - cntArr[l][ch - 'a']);
     }
   }  
 }
