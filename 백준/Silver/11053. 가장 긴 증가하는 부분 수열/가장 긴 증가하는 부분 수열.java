0467906276914df393ac737cd470a29a94cfda6c
Jo-Chang <74034844+Jo-Chang@users.noreply.github.com>
2023-09-12 15:20:49 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 132 ms, Memory: 14372 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -2,16 +2,6 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
-  private int max(int[] arr) {
-    int maxNum = 0;
-
-    for (int mem : arr) {
-      maxNum = mem > maxNum ? mem : maxNum;
-    }
-
-    return maxNum;
-  }
-
   private void solution() throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -36,10 +26,61 @@ public class Main {
     bw.write(String.valueOf(max(dp)));
 
     bw.flush();
+    bw.close();
+  }
 
+  private int max(int[] arr) {
+    int maxNum = 0;
+
+    for (int mem : arr) {
+      maxNum = mem > maxNum ? mem : maxNum;
+    }
+
+    return maxNum;
+  }
+
+  private void solution2() throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int n = Integer.parseInt(br.readLine());
+    int[] arr = new int[n], lis = new int[n];
+    
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    for (int i = 0; i < n; i++) {
+      arr[i] = Integer.parseInt(st.nextToken());
+    }
+
+    int idx = 0;
+    lis[idx] = arr[0];
+    for (int i = 1; i < n; i++) {
+      if (arr[i] > lis[idx]) {
+        lis[++idx] = arr[i];
+      } else {
+        lis[getBinarySearchIndex(lis, 0, idx, arr[i])] = arr[i];
+      }
+    }
+
+    System.out.println(idx + 1);
+  }
+
+  private int getBinarySearchIndex(int[] lis, int start, int end, int find) {
+    int mid;
+
+    while (start < end) {
+      mid = (start + end) / 2;
+
+      if (find > lis[mid]) {
+        start = mid + 1;
+      } else {
+        end = mid;
+      }
+    }
+
+    return end;
   }
 
   public static void main(String[] args) throws IOException{
-    new Main().solution();  
+    // new Boj11053().solution();  
+    new Main().solution2();  
   }
 }
