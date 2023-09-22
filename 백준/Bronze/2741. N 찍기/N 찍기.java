143182c156a369cbc32f96a1a3107bb63d705647
Jo-Chang <74034844+Jo-Chang@users.noreply.github.com>
2023-09-22 16:36:30 +0900

    [Bronze V] Title: N 찍기, Time: 236 ms, Memory: 22544 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+  BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  StringBuilder sb = new StringBuilder();
+
+  void solution() throws IOException {
+    int n = Integer.parseInt(br.readLine());
+
+    for (int i = 1; i <= n; i++) {
+      sb.append(String.valueOf(i)).append("\n");
+    }
+
+    System.out.println(sb);
+  }
+
+  public static void main(String[] args) throws IOException {
+    new Main().solution();
+  }
+}
