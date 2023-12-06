d26c66f73776b6101608d66ad8ee531793f73c44
Jo-Chang <74034844+Jo-Chang@users.noreply.github.com>
2023-12-06 17:22:43 +0900

    [Silver II] Title: 나무 자르기, Time: 540 ms, Memory: 119344 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -10,65 +10,32 @@ class Main {
     StringTokenizer st = new StringTokenizer(br.readLine());
     int n = Integer.parseInt(st.nextToken()), m = Integer.parseInt(st.nextToken());
     int[] woods = new int[n];
-    int answer = 0;
+    int max = 0;
     st = new StringTokenizer(br.readLine());
     for (int i = 0; i < n; i++) {
       woods[i] = Integer.parseInt(st.nextToken());
+      max = Math.max(max, woods[i]);
     }
     br.close();
 
-    Arrays.sort(woods);
-
-    int startIdx = 0, endIdx = n, midIdx = (startIdx + endIdx) / 2;
-    long cnt = 0;
-    while (startIdx < endIdx) {
-      cnt = 0;
-      for (int i = midIdx; i < n; i++) {
-        if (woods[i] > woods[midIdx]) cnt += woods[i] - woods[midIdx];
-      }
-
-      if (cnt > m) {
-        startIdx = midIdx + 1;
-      } else if (cnt < m) {
-        endIdx = midIdx;
-      } else {
-        break;
-      }
-
-      midIdx = (startIdx + endIdx) / 2;
-    }
-
-    int start = 0;
-    if (midIdx > 0) start = woods[midIdx - 1];
-    int end = woods[midIdx];
-    if (midIdx < n - 1) end = woods[midIdx + 1];
-    int mid = 0;
-    while (start < end) {
-      mid = (start + end) / 2;
-      
-      cnt = 0;
-      for (int i = midIdx; i < n; i++) {
+    int start = 0, end = max, mid = 0;
+    while (start <= end) {
+      long cnt = 0;
+      for (int i = 0; i < n; i++) {
         if (woods[i] > mid) cnt += woods[i] - mid;
       }
       
       if (cnt > m) {
         start = mid + 1;
       } else if (cnt < m) {
-        end = mid;
+        end = mid - 1;
       } else {
         break;
       }
-    }
-    answer = mid;
 
-    while (cnt < m) {
-      answer--;
-      cnt = 0;
-      for (int i = midIdx; i < n; i++) {
-        if (woods[i] > mid) cnt += woods[i] - answer;
-      }
+      mid = (start + end) / 2;
     }
 
-    System.out.println(answer);
+    System.out.println(mid);
   }
 }
