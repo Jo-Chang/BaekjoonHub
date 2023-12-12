f5283e75532c42a7f6bab967e801b5f3cff2eb86
Jo-Chang <74034844+Jo-Chang@users.noreply.github.com>
2023-12-12 10:49:24 +0900

    [Gold V] Title: 토마토, Time: 924 ms, Memory: 275032 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.java
+++ 백준/Gold/7569. 토마토/토마토.java
@@ -1,8 +1,9 @@
 import java.io.*;
 import java.util.*;
 
-class Main {
+public class Main {
   static final boolean DEBUG = false;
+
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     int answer = 0;
@@ -13,81 +14,77 @@ class Main {
     int N = Integer.parseInt(st.nextToken());
     int H = Integer.parseInt(st.nextToken());
     int[][][] tomatoes = new int[H][N][M];
+    ArrayList<Position> unripped = new ArrayList<>();
+    ArrayList<Position> ripped = new ArrayList<>();
     for (int i = 0; i < H; i++) {
       for (int j = 0; j < N; j++) {
         st = new StringTokenizer(br.readLine());
         for (int k = 0; k < M; k++) {
           tomatoes[i][j][k] = Integer.parseInt(st.nextToken());
+          if (tomatoes[i][j][k] == 0) {
+            unripped.add(new Position(i, j, k));
+          } else if (tomatoes[i][j][k] == 1) {
+            ripped.add(new Position(i, j, k));
+          }
         }
       }
     }
 
     // 토마토 숙성도 변화
-    changeTomatoes(tomatoes);
+    answer = changeTomatoes(tomatoes, ripped);
 
     // 안 익은 토마토 확인
-    answer = checkAllTomatoes(tomatoes);
+    answer = checkUnripped(tomatoes, unripped, answer);
 
     // Debug
-    if (DEBUG) printTomatoes(tomatoes);
+    if (DEBUG)
+      printTomatoes(tomatoes);
 
     System.out.println(answer);
     br.close();
   }
 
-  static void changeTomatoes(int[][][] tomatoes) {
-    Deque<Integer> q = new LinkedList<>();
-    int[][] delta = {{1, 0, 0}, {-1, 0, 0}, {0, 1, 0}, {0, -1, 0}, {0, 0, 1}, {0, 0, -1}};
+  static int changeTomatoes(int[][][] tomatoes, ArrayList<Position> ripped) {
+    int result = 0;
+    Queue<Position> q = new LinkedList<>();
+    int[][] delta = { { 1, 0, 0 }, { -1, 0, 0 }, { 0, 1, 0 }, { 0, -1, 0 }, { 0, 0, 1 }, { 0, 0, -1 } };
     int h = tomatoes.length;
     int n = tomatoes[0].length;
     int m = tomatoes[0][0].length;
 
-    for (int i = 0; i < h; i++) {
-      for (int j = 0; j < n; j++) {
-        for (int k = 0; k < m; k++) {
-          if (tomatoes[i][j][k] == 1) {
-            q.add(i);
-            q.add(j);
-            q.add(k);
-            while (!q.isEmpty()) {
-              int a = q.remove();
-              int b = q.remove();
-              int c = q.remove();
+    for (Position pos : ripped) {
+      q.add(pos);
+    }
+    while (!q.isEmpty()) {
+      Position p = q.remove();
+      result = Math.max(tomatoes[p.h][p.n][p.m] - 1, result);
 
-              for (int[] dt : delta) {
-                int na = a + dt[0];
-                int nb = b + dt[1];
-                int nc = c + dt[2];
+      for (int[] dt : delta) {
+        Position np = new Position(p.h + dt[0], p.n + dt[1], p.m + dt[2]);
 
-                if (0 <= na && na < h && 0 <= nb && nb < n && 0 <= nc && nc < m) {
-                  if (tomatoes[na][nb][nc] == 0 || tomatoes[na][nb][nc] > tomatoes[a][b][c] + 1) {
-                    tomatoes[na][nb][nc] = tomatoes[a][b][c] + 1;
-                    q.add(na);
-                    q.add(nb);
-                    q.add(nc);
-                  }
-                }
-              }
+        if (0 <= np.h && np.h < h && 0 <= np.n && np.n < n && 0 <= np.m && np.m < m) {
+          int cnt = tomatoes[p.h][p.n][p.m] + 1;
+          if (tomatoes[np.h][np.n][np.m] == 0) {
+            if (DEBUG) {
+              System.out.println("Debug!");
+              System.out.println(np.h + ", " + np.n + ", " + np.m);
             }
-
-            q.clear();
+            tomatoes[np.h][np.n][np.m] = cnt;
+            q.add(np);
           }
         }
       }
-    }
-  }
 
-  static int checkAllTomatoes(int[][][] tomatoes) {
-    int result = 0;
-    for (int i = 0; i < tomatoes.length; i++) {
-      for (int j = 0; j < tomatoes[0].length; j++) {
-        for (int k = 0; k < tomatoes[0][0].length; k++) {
-          result = Math.max(result, tomatoes[i][j][k] - 1);
-          if (tomatoes[i][j][k] == 0) return -1;
-        }
-      }
     }
     return result;
+  }
+
+  static int checkUnripped(int[][][] tomatoes, ArrayList<Position> unripped, int answer) {
+    for (Position p : unripped) {
+      if (tomatoes[p.h][p.n][p.m] == 0)
+        return -1;
+    }
+    return answer;
   }
 
   static void printTomatoes(int[][][] tomatoes) {
@@ -102,5 +99,22 @@ class Main {
       System.out.println("==========");
     }
     System.out.println("Debug End");
+  }
+}
+
+class Position {
+  int n, m, h;
+
+  public Position() {
+    this.n = 0;
+    this.m = 0;
+    this.h = 0;
+  }
+
+  public Position(int h, int n, int m) {
+    super();
+    this.n = n;
+    this.m = m;
+    this.h = h;
   }
 }
