8b28d7c50d43b56b6494c5346ec3172a321f3036
Jo-Chang <74034844+Jo-Chang@users.noreply.github.com>
2024-01-05 10:31:37 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+    int N;
+    
+    scanf("%d", &N);
+    
+    for (int i = 0; i < N / 4; i++) {
+        printf("long ");
+    }
+    printf("int");
+    
+    return 0;
+}
