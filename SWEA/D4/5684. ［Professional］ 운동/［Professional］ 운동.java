27a783df4489e470b07acd204902cbda2d34f6d5
Jo-Chang <74034844+Jo-Chang@users.noreply.github.com>
2024-01-20 11:29:01 +0900

    [D4] Title: [Professional] 운동, Time: 1,653 ms, Memory: 78,384 KB -BaekjoonHub

--- SWEA/D4/5684. ［Professional］ 운동/［Professional］ 운동.java
+++ SWEA/D4/5684. ［Professional］ 운동/［Professional］ 운동.java
@@ -1,55 +1,102 @@
 import java.io.*;
 import java.util.*;
 
+
 class Solution {
+  final int INF = Integer.MAX_VALUE;
+
+  int[][] graph;
+  int N, M;
+  
   public static void main(String[] args) throws IOException {
+    new Solution().sol();
+  }
+
+  void sol() throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
     StringBuilder sb = new StringBuilder();
 
     int T = Integer.parseInt(br.readLine());
     for (int tc = 1; tc <= T; tc++) {
-      int answer = -1;
-      StringTokenizer st = new StringTokenizer(br.readLine());
-      int N = Integer.parseInt(st.nextToken());
-      int M = Integer.parseInt(st.nextToken());
-      int[][] graph = new int[N + 1][N + 1];
-      int[][] distance = new int[N + 1][N + 1];
+      st = new StringTokenizer(br.readLine());
+
+      N = Integer.parseInt(st.nextToken());
+      M = Integer.parseInt(st.nextToken());
+
+      graph = new int[N + 1][N + 1];
+      for (int i = 1; i <= N; i++) {
+        Arrays.fill(graph[i], INF);
+      }
+
       for (int i = 0; i < M; i++) {
         st = new StringTokenizer(br.readLine());
+
         int S = Integer.parseInt(st.nextToken());
         int E = Integer.parseInt(st.nextToken());
         int C = Integer.parseInt(st.nextToken());
-        graph[S][E] = C;
-        distance[S][E] = C;
-      }
-      for (int i = 1; i <= N; i++) {
-        for (int start = 1; start <= N; start++) {
-          if (start == i) continue;
-          for (int end = 1; end <= N; end++) {
-            if (end == i || end == start) continue;
-            if (distance[start][i] == 0 || distance[i][end] == 0) continue;
 
-            if (distance[start][end] == 0) distance[start][end] = distance[start][i] + distance[i][end];
-            else distance[start][end] = Math.min(distance[start][end], distance[start][i] + distance[i][end]);
-          }
-        }
+        graph[S][E] = C;
       }
+      
+      dijkstra();
+
+      int answer = INF;
       for (int i = 1; i <= N; i++) {
+        if (graph[i][i] != 0) answer = Math.min(answer, graph[i][i]);
         for (int j = 1; j <= N; j++) {
-          if (i == j || graph[j][i] == 0 || distance[i][j] == 0) continue;
-          int newDistance = distance[i][j] + graph[j][i];
-          distance[i][i] = (distance[i][i] == 0) ? newDistance : Math.min(distance[i][i], newDistance);
+          if (graph[i][j] == INF || graph[j][i] == INF || i == j) continue;
+
+          answer = Math.min(answer, graph[i][j] + graph[j][i]);
         }
       }
-      for (int i = 1; i <= N; i++) {
-        if (distance[i][i] == 0) continue;
-        
-        answer = (answer == -1) ? distance[i][i] : Math.min(answer, distance[i][i]);
-      }
-      sb.append(String.format("#%d %d\n", tc, answer));
+      sb.append(String.format("#%d %d\n", tc, (answer == INF) ? -1 : answer));
     }
     br.close();
 
     System.out.println(sb);
+  }
+
+  void dijkstra() {
+    for (int i = 1; i <= N; i++) {
+      PriorityQueue<Node> pq = new PriorityQueue<>();
+      pq.add(new Node(i, 0));
+
+      int[] distance = new int[N + 1];
+      Arrays.fill(distance, INF);
+      
+      while (!pq.isEmpty()) {
+        Node node = pq.remove();
+        int cd = node.destination;
+        int cw = node.weight;
+
+        for (int j = 1; j <= N; j++) {
+          if (graph[cd][j] == INF) continue;
+          
+          int newWeight = cw + graph[cd][j];
+          if (distance[j] > newWeight) {
+            distance[j] = newWeight;
+            pq.add(new Node(j, newWeight));
+          }
+        }
+      }
+
+      graph[i] = distance;
+    }
+  }
+
+}
+
+class Node implements Comparable<Node> {
+  int destination, weight;
+
+  Node(int destination, int weight) {
+    this.destination = destination;
+    this.weight = weight;
+  }
+
+  @Override
+  public int compareTo(Node arg) {
+    return weight - arg.weight;
   }
 }
