f13f2b6f76d9d22647bfa8805d7874fb58116121
Jo-Chang <74034844+Jo-Chang@users.noreply.github.com>
2024-03-18 17:04:02 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 80 ms, Memory: 11464 KB -BaekjoonHub

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.java
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.java
@@ -2,48 +2,72 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-  public static void main(String[] args) throws IOException {
-    new BOJ11054().sol();
-  }
-}
 
-class BOJ11054 {
-  void sol() throws IOException {
-    int N = readInt();
-    int[] arr = new int[N];
-    for (int i = 0; i < N; i++) arr[i] = readInt();
-
-    int[][] dp = new int[N][2];
-    // dp[i][0] -> Increasing
-    // dp[i][1] -> Decreasing
-    for (int i = 0; i < N; i++) {
-      dp[i][0] = dp[i][1] = 1;
-      
-      // Increasing
-      for (int j = 0; j < i; j++) {
-        if (arr[i] > arr[j]) dp[i][0] = Math.max(dp[i][0], dp[j][0] + 1);
-      }
-
-      // Decreasing
-      for (int j = 0; j < i; j++) {
-        if (arr[i] < arr[j]) dp[i][1] = Math.max(dp[i][1], Math.max(dp[j][0], dp[j][1]) + 1);
-      }
+    private static int readInt() throws IOException{
+        int c, n = 0;
+        while((c = System.in.read()) > 32)
+            n = (n << 3) + (n << 1) + (c & 15);
+        return n;
     }
     
-    int answer = 0;
-    for (int i = 0; i < N; i++) answer = Math.max(answer, Math.max(dp[i][0], dp[i][1]));
-    System.out.println(answer);
-  }
+    private static int binarySearch(int[] arr, int key, int size){
+        int l = 0, r = size;
 
-  int readInt() throws IOException {
-    int input = System.in.read();
-    int num = 0;
+        while (l < r){
+            int m = (l + r) / 2;
 
-    while (input > 32) {
-      num = (num << 3) + (num << 1) + (input & 15);
-      input = System.in.read();
+            if (arr[m] >= key){
+                r = m;
+            }
+            else{
+                l = m + 1;
+            }
+        }
+        return r;
     }
 
-    return num;
-  }
+    public static void main(String[] args) throws IOException {
+        int N = readInt();
+
+        /** input arr */
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++){
+            arr[i] = readInt();
+        }
+
+        /** 최장 증가 부분수열 기록 */
+        int[] increase = new int[N];
+        int[] dp_LIS = new int[N];      //Longest Increasing Subsequence 길이
+        int size = 1;
+        increase[0] = arr[0];
+        dp_LIS[0] = 1;
+        for (int i = 1; i < N; i++){
+            int idx = binarySearch(increase, arr[i], size);
+            increase[idx] = arr[i];         //DP(LIS 기록)
+            dp_LIS[i] = idx + 1;            //해당 인덱스(i)가 포함되는 LIS 최대 길이(idx+1) 기록
+            size = Math.max(size, idx + 1); //이분탐색 범위 업데이트
+        }
+
+        /** 최장 감소 부분수열(역방향) 기록*/
+        int[] decrease = new int[N];
+        int[] dp_LDS = new int[N];      //Longest Decreasing Subsequence 길이
+        size = 1;
+        decrease[0] = arr[N - 1];
+        dp_LDS[0] = 1;
+        for (int i = 1; i < N; i++){
+            int idx = binarySearch(decrease, arr[N - i - 1], size);
+            decrease[idx] = arr[N - i - 1]; //DP(LDS 기록)
+            dp_LDS[N - i - 1] = idx + 1;    //해당 인덱스(N-i-1)가 포함되는 LDS 최대 길이(idx+1) 기록
+            size = Math.max(size, idx + 1); //이분탐색 범위 업데이트
+        }
+
+        /** 최장 바이토닉 부분수열 길이 구하기 */
+        int maxLen = 0;
+        for (int i = 0; i < N; i++){
+            //i: 증가에서 감소로 바뀌는 부분
+            maxLen = Math.max(maxLen, dp_LIS[i] + dp_LDS[i] - 1);
+        }
+        System.out.println(maxLen);
+    }
+
 }
