ed22d8c3f1ace2957b97eeebe45f9e247a1da0ad
Jo-Chang <74034844+Jo-Chang@users.noreply.github.com>
2024-09-04 18:47:12 +0900

    [Silver IV] Title: solved.ac, Time: 248 ms, Memory: 21780 KB -BaekjoonHub

--- 백준/Silver/18110. solved．ac/solved．ac.java
+++ 백준/Silver/18110. solved．ac/solved．ac.java
@@ -7,7 +7,17 @@ public class Main {
   final double RATIO = 0.15;
 
   int readInt() throws IOException {
-    return Integer.parseInt(br.readLine());
+    int n = 0;
+
+    while (true) {
+      int input = System.in.read();
+      if (input <= 32) {
+        //  입력이 공백인 경우
+        return n;
+      } else {
+        n = (n << 3) + (n << 1) + (input & 15);
+      }
+    }
   }
 
   void solution() throws IOException {
@@ -31,7 +41,30 @@ public class Main {
     System.out.println(answer);
   }
 
+  void solution2() throws IOException {
+    int n = readInt();
+    int ex = (int)Math.round(n * RATIO);
+    int len = n - 2 * ex;
+    PriorityQueue<Integer> q = new PriorityQueue<>();
+
+    for (int i = 0; i < n; i++) {
+      q.add(readInt());
+    }
+
+    for (int i = 0; i < ex; i++) {
+      q.poll();
+    }
+
+    int sum = 0;
+    for (int i = 0; i < len; i++) {
+      sum += q.poll();
+    }
+
+    System.out.println(Math.round((double)sum / len));
+  }
+
   public static void main(String[] args) throws IOException {
     new Main().solution();
+    // new Main().solution2();
   }
 }
