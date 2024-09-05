5bd53a5b7a7a2a96b01a4e325d38d52233e6cea1
Jo-Chang <74034844+Jo-Chang@users.noreply.github.com>
2024-09-05 10:05:34 +0900

    [Bronze V] Title: 새싹, Time: 96 ms, Memory: 13876 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+
+	public static void main(String[] args) throws IOException {
+		String str = 
+				"""
+				         ,r'"7
+				r`-_   ,'  ,/
+				 \\. ". L_r'
+				   `~\\/
+				      |
+				      |				
+				""";
+		
+		System.out.println(str);
+	}
+	
+}
