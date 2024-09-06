4f1f5a5964bc12a41af8cec4de76d88281c7e6b5
Jo-Chang <74034844+Jo-Chang@users.noreply.github.com>
2024-09-06 09:43:48 +0900

    [Bronze V] Title: A×B, Time: 96 ms, Memory: 12868 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+class Main {
+
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		System.out.println(a * b);
+	}
+	
+}
