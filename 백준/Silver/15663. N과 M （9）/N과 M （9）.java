391b3ee48db3b3f57a0c72eaf4025dfd18aa0386
Jo-Chang <74034844+Jo-Chang@users.noreply.github.com>
2024-10-11 14:43:22 +0900

    [Silver II] Title: N과 M (9), Time: 120 ms, Memory: 18056 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.java
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -2,73 +2,58 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	static Set<String> combinations = new HashSet<>();
+
 	static StringBuilder sb = new StringBuilder();
-	
+	static int[] numbers, comb;
+	static int n, m;
+
 	public static void main(String[] args) throws IOException {
-	
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-		
-		
-		int[] numbers = new int[n];
+
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+
+		numbers = new int[n];
+		comb = new int[m];
 		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < n; i++) {
 			numbers[i] = Integer.parseInt(st.nextToken());
 		}
-		
+
 		Arrays.sort(numbers);
-		
-		addDistinctCombination(numbers, new int[m], 0);
-		
+
+		addDistinctPermutation(0);
+
 		System.out.println(sb);
-		
+
 	}
-	
-	static void addDistinctCombination(int[] arr, int[] comb, int depth) {
-		if (depth == comb.length) {
-			String combination = myToString(comb);
-			
-			if (!combinations.contains(combination)) {
-				combinations.add(combination);
-				
-				for (int i = 0; i < comb.length; i++) {
-					sb.append(comb[i]).append(" ");
-				}
-				
-				sb.append("\n");
+
+	static void addDistinctPermutation(int depth) {
+		if (depth == m) {
+			for (int i = 0; i < m; i++) {
+				sb.append(comb[i]).append(" ");
 			}
-			
+			sb.append("\n");
+
 			return;
 		}
-		
-		for (int i = 0; i < arr.length; i++) {
-			if (arr[i] == 0) continue;
-			
-			comb[depth] = arr[i];
-			arr[i] = 0;
-			addDistinctCombination(arr, comb, depth + 1);
-			arr[i] = comb[depth];
+
+		int prev = 0;
+		for (int i = 0; i < n; i++) {
+			if (numbers[i] == 0 || numbers[i] == prev)
+				continue;
+
+			comb[depth] = numbers[i];
+			prev = numbers[i];
+			numbers[i] = 0;
+			addDistinctPermutation(depth + 1);
+			numbers[i] = comb[depth];
 		}
 	}
-	
-	static String myToString(int[] arr) {
-		StringBuilder sb = new StringBuilder();
-		
-		for (int num : arr) {
-			sb.append(num).append(" ");
-		}
-		
-		return sb.toString();
-	}
-	
+
 }
