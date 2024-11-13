97454625342a63c75978ded45b501b4b4ce006af
Jo-Chang <74034844+Jo-Chang@users.noreply.github.com>
2024-11-13 13:53:21 +0900

    [Bronze I] Title: 피보나치 수, Time: 108 ms, Memory: 15508 KB -BaekjoonHub

--- 백준/Bronze/4150. 피보나치 수/피보나치 수.java
+++ 백준/Bronze/4150. 피보나치 수/피보나치 수.java
@@ -1,74 +1,25 @@
 import java.io.*;
+import java.math.BigInteger;
 import java.util.*;
 
 class Main {
-	
-	static String[] fiboNums = new String[5001];
 	
 	public static void main(String[] args) throws IOException {
 		
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		System.out.println(fibonacci(n));
+		BigInteger[] fiboNums = new BigInteger[n + 1];
 		
-	}
-	
-	static String fibonacci(int n) {
-		if (fiboNums[n] != null) return fiboNums[n];
-		if (n == 1 || n == 2) return fiboNums[n] = "1";
-		return fiboNums[n] = stringSum(fibonacci(n - 1), fibonacci(n - 2));
-	}
-	
-	static String stringSum(String n1, String n2) {
-		StringBuffer sb = new StringBuffer();
-		boolean isOver = false;
-		int idx = 0, len1 = n1.length(), len2 = n2.length();
-
-		while (idx < len1 && idx < len2) {
-			char newChar = (char) (n1.charAt(len1 - 1 - idx) + n2.charAt(len2 - 1 - idx) - '0');
-			if (isOver) {
-				newChar++;
-				isOver = false;
-			}
-			if (newChar > '9') {
-				newChar -= 10;
-				isOver = true;
-			}
-			sb.append(String.valueOf(newChar));
-			idx++;
+		fiboNums[0] = BigInteger.valueOf(0);
+		fiboNums[1] = BigInteger.valueOf(1);
+		if (n > 1) fiboNums[2] = BigInteger.valueOf(1);
+		
+		for (int i = 3; i <= n; i++) {
+			fiboNums[i] = fiboNums[i - 1].add(fiboNums[i - 2]);
 		}
 		
-		while (idx < len1) {
-			char newChar = n1.charAt(len1 - 1 - idx);
-			if (isOver) {
-				newChar++;
-				isOver = false;
-			}
-			if (newChar > '9') {
-				newChar -= 10;
-				isOver = true;
-			}
-			sb.append(String.valueOf(newChar));
-			idx++;
-		}
+		System.out.println(fiboNums[n]);
 		
-		while (idx < len2) {
-			char newChar = n2.charAt(len2 - 1 - idx);
-			if (isOver) {
-				newChar++;
-				isOver = false;
-			}
-			if (newChar > '9') {
-				newChar -= 10;
-				isOver = true;
-			}
-			sb.append(String.valueOf(newChar));
-			idx++;
-		}
-		
-		if (isOver) sb.append("1");
-		
-		return sb.reverse().toString();
 	}
 	
 }
