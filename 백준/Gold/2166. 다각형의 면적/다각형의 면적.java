b5583c7f926b6c6939cdeed8c771e49c48ea8352
Jo-Chang <74034844+Jo-Chang@users.noreply.github.com>
2025-02-14 20:18:42 +0900

    [Gold V] Title: 다각형의 면적, Time: 88 ms, Memory: 12708 KB -BaekjoonHub

---
+++ 백준/Gold/2166. 다각형의 면적/다각형의 면적.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        new Main().sol();
+    }
+
+    void sol() throws IOException {
+        int n = read();
+        int[][] arr = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            arr[i][0] = read();
+            arr[i][1] = read();
+        }
+
+        System.out.printf("%.1f", getArea(arr));
+    }
+
+    double getArea(int[][] arr) {
+        long area = 0;
+        int[] prev = arr[arr.length - 1];
+
+        for (int i = 0; i < arr.length; i++) {
+//            area += ((long) prev[0] + arr[i][0]) * (prev[1] - arr[i][1]);
+            area += (long) prev[0] * arr[i][1] - (long) arr[i][0] * prev[1];
+            prev = arr[i];
+        }
+
+        return (double) Math.abs(area) / 2;
+    }
+
+    int read() throws IOException {
+        int c = System.in.read(), n = 0, minus = 1;
+
+        if (c == '-') minus = -1;
+        else n = c & 15;
+
+        while ((c = System.in.read()) > 32) n = (n<<3) + (n<<1) + (c&15);
+
+        if (c == 13) System.in.read();
+
+        return n * minus;
+    }
+
+}
