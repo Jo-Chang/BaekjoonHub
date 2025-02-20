5507e1f034ef96dc2f8693ea9ad81d516bdfb3ed
Jo-Chang <74034844+Jo-Chang@users.noreply.github.com>
2025-02-20 12:13:39 +0900

    [Bronze III] Title: 최소, 최대, Time: 236 ms, Memory: 12236 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,6 +1,5 @@
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -11,29 +10,25 @@ public class Main {
     }
 
     void sol() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = null;
-        int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
+        int n = read(), min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
 
-        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            int num = read();
+            min = Math.min(min, num);
+            max = Math.max(max, num);
         }
 
-        System.out.printf(
-                "%d %d",
-                Arrays.stream(arr).min().getAsInt(),
-                Arrays.stream(arr).max().getAsInt()
-        );
+        System.out.println(min + " " + max);
     }
 
     int read() throws IOException {
-        int c, n = 0;
+        int c = System.in.read(), n = 0, minus = 1;
 
+        if (c == '-') minus = -1;
+        else n = c&15;
         while ((c = System.in.read()) > 32) n = (n<<3) + (n<<1) + (c&15);
 
-        return n;
+        return n * minus;
     }
 
 }
