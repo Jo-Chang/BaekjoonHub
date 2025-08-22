f75528d8d2f52bf0990d10b61e80ca34b93435e9
Jo-Chang <74034844+Jo-Chang@users.noreply.github.com>
2025-08-22 18:12:39 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 1788 ms, Memory: 337800 KB -BaekjoonHub

---
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
@@ -0,0 +1,86 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+class Edge implements Comparable<Edge> {
+    int u, v, w;
+
+    Edge(int u, int v, int w) {
+        this.u = u;
+        this.v = v;
+        this.w = w;
+    }
+
+    @Override
+    public int compareTo(Edge other) {
+        return Integer.compare(this.w, other.w);
+    }
+
+    @Override
+    public String toString() {
+        return "Edge{" +
+                "u=" + u +
+                ", v=" + v +
+                ", w=" + w +
+                '}';
+    }
+}
+
+public class Main {
+
+    static int N, M;
+    static List<Integer> disjointSet;
+    static List<Edge> edges;
+    static List<Edge> resultEdge; // 최소 신장 트리 간선을 파악 위함
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        disjointSet = IntStream.rangeClosed(0, N).boxed().collect(Collectors.toList());
+        edges = new ArrayList<>();
+        resultEdge = new ArrayList<>();
+        for (int m = 0; m < M; m++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+            edges.add(new Edge(u, v, weight));
+        }
+
+        Collections.sort(edges);
+
+        for (Edge e : edges) {
+            if (findSet(e.u) != findSet(e.v)) {
+                resultEdge.add(e);
+                unionSet(e.u, e.v);
+            }
+        }
+
+        int result = 0;
+        for (Edge e : resultEdge.subList(0, resultEdge.size() - 1)) result += e.w;
+
+        System.out.print(result);
+    }
+
+    static int findSet(int a) {
+        if (disjointSet.get(a) != a)
+            disjointSet.set(a, findSet(disjointSet.get(a)));
+        return disjointSet.get(a);
+    }
+
+    static void unionSet(int a, int b) {
+        int rootA = findSet(a);
+        int rootB = findSet(b);
+        disjointSet.set(rootB, rootA);
+    }
+
+}
